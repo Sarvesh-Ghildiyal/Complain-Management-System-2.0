445fb9fa0f276c494990ad54a8ec56d98f4e5a73
Sarvesh-Ghildiyal <sarveshghildiyal@gmail.com>
2023-08-13 09:59:29 +0530

    multi-auth done

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id()){
+            $role=Auth()->user()->role;
+            
+            if($role=='user'){
+                return view('dashboard');
+            }
+
+            elseif($role=='admin'){
+                return view('admin.adminDashboard');
+            }
+            
+            elseif($role=='worker'){
+                return view('worker.workerDashboard');
+            }
+
+            else{
+                // Logout the user
+                Auth::logout();
+                // Clear the session
+                session()->flush();
+                return redirect()->back();
+            }
+        }
+    }
+}
