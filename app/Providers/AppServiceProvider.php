c978e8b1c31c450da00231d072635fa4b97122a6
Ravinder <raviarya4542@gmail.com>
2023-10-28 11:15:14 +0530

    sidebar link and registration form done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Blade::component('select-input', \App\View\Components\SelectInput::class);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,25 +5,26 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -32,15 +33,67 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role Selection -->
+        <div class="mt-4">
+            <x-input-label for="role" :value="__('Role')" />
+            <div class="flex text-white">
+                <label class="block mr-4">
+                    <input type="radio" name="role" value="admin">
+                    Admin
+                </label>
+                <label class="block mr-4">
+                    <input type="radio" name="role" value="user">
+                    User
+                </label>
+                <label class="block">
+                    <input type="radio" name="role" value="worker">
+                    Worker
+                </label>
+            </div>
+        </div>
+
+        <!-- DropDown-->
+        <div class="mt-4">
+            <x-input-label for="department" :value="__('Department')" />
+            <div class="flex text-white">
+                <label class="block mr-4">
+                    <input type="radio" name="department" value="CSE">
+                    CSE
+                </label>
+                <label class="block mr-4">
+                    <input type="radio" name="department" value="AI/ML">
+                    AI/ML
+                </label>
+                <label class="block">
+                    <input type="radio" name="department" value="CE">
+                    CE
+                </label>
+                <label class="block">
+                    <input type="radio" name="department" value="ME">
+                    ME
+                </label>
+                <label class="block">
+                    <input type="radio" name="department" value="ECE">
+                    ECE
+                </label>
+                <label class="block">
+                    <input type="radio" name="department" value="EE">
+                    EE
+                </label>
+            </div>
+        </div>
+
+
+
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
